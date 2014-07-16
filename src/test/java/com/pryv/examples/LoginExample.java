803741dcad02db33fce16a15e8830044f632b71c
ikebets <ilia.kebets@epfl.ch>
2014-07-16 14:47:38 +0200

    add dummy permission to login example

--- src/test/java/com/pryv/examples/LoginExample.java
+++ src/test/java/com/pryv/examples/LoginExample.java
@@ -20,9 +20,15 @@ public class LoginExample {
   public static void main(String[] args) {
 
     String reqAppId = "web-page-test";
-    List<Permission> permissions = new ArrayList<Permission>();
     String lang = "en";
     String returnURL = "fakeURL";
+
+    List<Permission> permissions = new ArrayList<Permission>();
+    String streamId1 = "pics";
+    Permission.Level perm1 = Permission.Level.contribute;
+    String defaultName1 = "ddd";
+    Permission testPermission1 = new Permission(streamId1, perm1, defaultName1);
+    permissions.add(testPermission1);
 
     Pryv.setStaging();
     LoginController authenticator = new Authenticator(reqAppId, permissions, lang, returnURL);
