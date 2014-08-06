843ada4013503dd0c053a8e92a971d0de0e24ce7
ikebets <ilia.kebets@epfl.ch>
2014-08-06 10:50:38 +0200

    add test credentials.

---
+++ src/test/java/resources/TestCredentials.java
@@ -0,0 +1,14 @@
+package resources;
+
+/**
+ *
+ * DO NOT PUSH on repo
+ *
+ *
+ * @author ik
+ *
+ */
+public class TestCredentials {
+  public final static String USERNAME = "perkikiki";
+  public final static String TOKEN = "chu2cc0zy0000iawkyrw7g9gq";
+}
