cf9675619380c3ac7198828ab83f9d7e1b3c6526
ikebets <ilia.kebets@epfl.ch>
2014-08-08 18:20:13 +0200

    add mockito library for asynchronous integration testing.

--- src/test/java/com/pryv/functional/RetrieveEventsTest.java
+++ src/test/java/com/pryv/functional/RetrieveEventsTest.java
@@ -1,9 +1,12 @@
 package com.pryv.functional;
 
+import static org.junit.Assert.assertTrue;
+
 import java.util.Map;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mock;
 
 import resources.TestCredentials;
 
@@ -25,39 +28,46 @@ public class RetrieveEventsTest {
 
   private EventsManager<Map<String, Event>> connection;
 
+  @Mock
+  private EventsCallback<Map<String, Event>> testCallback =
+    new EventsCallback<Map<String, Event>>() {
+
+    @Override
+    public void onEventsSuccess(Map<String, Event> events) {
+      System.out.println("success");
+      assertTrue(events.values().size() > 0);
+    }
+
+    @Override
+    public void onEventsPartialResult(Map<String, Event> newEvents) {
+      // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void onEventsError(String message) {
+      // TODO Auto-generated method stub
+
+    }
+  };
+
+
+
   @Before
   public void setUp() throws Exception {
     // create Connection with valid username/token for testing
     Pryv.setStaging();
     connection = new Connection(TestCredentials.USERNAME, TestCredentials.TOKEN);
-
   }
 
   @Test
+
   /**
    * test fetching of events
    */
   public void testFetchEvents() {
-    connection.getEvents(new Filter(), new EventsCallback<Map<String, Event>>() {
-
-      @Override
-      public void onEventsSuccess(Map<String, Event> events) {
-        // assertTrue();
-
-      }
-
-      @Override
-      public void onEventsPartialResult(Map<String, Event> newEvents) {
-        // TODO Auto-generated method stub
-
-      }
-
-      @Override
-      public void onEventsError(String message) {
-        // TODO Auto-generated method stub
-
-      }
-    });
+    System.out.println("testFetchEvents");
+    connection.getEvents(new Filter(), testCallback);
   }
 
 }
