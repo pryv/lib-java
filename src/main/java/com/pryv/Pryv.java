43fe4379cb97b147e77ce75317c580498829d326
ikebets <ilia.kebets@epfl.ch>
2014-08-25 18:56:04 +0200

    add boolean variables to specify when various modules are used or not.

--- src/main/java/com/pryv/Pryv.java
+++ src/main/java/com/pryv/Pryv.java
@@ -13,15 +13,85 @@ public class Pryv {
   public static String API_DOMAIN = "pryv.io";
   public static final String DATABASE_NAME = "pryv-sqlite.db";
 
+  private static boolean cacheActivated = true;
+  private static boolean onlineActivated = true;
+  private static boolean supervisorActivated = true;
+
+  /**
+   * makes the library use staging mode from online.
+   */
   public static void setStaging() {
     REGISTRATION_URL = "https://reg.pryv.in/access";
     API_DOMAIN = "pryv.in";
   }
 
-  public static void getAuthorization() {
-
+  /**
+   * This function returns if the cache is activated or not.
+   *
+   * @return
+   */
+  public boolean isCacheActive() {
+    return cacheActivated;
   }
 
+  /**
+   * This function returns if the online module is activated or not.
+   *
+   * @return
+   */
+  public boolean isOnlineActive() {
+    return onlineActivated;
+  }
 
+  /**
+   * This function returns if the Supervisor is activated or not.
+   *
+   * @return
+   */
+  public boolean isSupervisorActive() {
+    return supervisorActivated;
+  }
+
+  /**
+   * activates the cache
+   */
+  public void activateCache() {
+    cacheActivated = true;
+  }
+
+  /**
+   * stop using the cache
+   */
+  public void deactivateCache() {
+    cacheActivated = false;
+  }
+
+  /**
+   * activates the online module
+   */
+  public void activateOnline() {
+    onlineActivated = true;
+  }
+
+  /**
+   * Stops using the online module
+   */
+  public void deactivateOnline() {
+    onlineActivated = false;
+  }
+
+  /**
+   * activates the Supervisor
+   */
+  public void activatedSupervisor() {
+    supervisorActivated = true;
+  }
+
+  /**
+   * stop using the Supervisor
+   */
+  public void deactivateSupervisor() {
+    supervisorActivated = false;
+  }
 
 }
