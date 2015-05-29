e69b2e283eadb0e22a7f07e1b6b81538d6db4c5e
ikebets <ilia.kebets@epfl.ch>
2015-05-29 10:34:32 +0200

    initial Connection test

---
+++ src/test/java/com/pryv/unit/ConnectionTest.java
@@ -0,0 +1,37 @@
+package com.pryv.unit;
+
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.pryv.Connection;
+import com.pryv.api.database.DBinitCallback;
+
+public class ConnectionTest {
+
+  private Connection connection;
+
+  @BeforeClass
+  public static void setUpBeforeClass() throws Exception {
+  }
+
+  @AfterClass
+  public static void tearDownAfterClass() throws Exception {
+  }
+
+  @Before
+  public void setUp() throws Exception {
+    connection = new Connection("testUID2", "token", new DBinitCallback());
+  }
+
+  @Test
+  public void testIdCachingGeneration() {
+    String idCaching = connection.getIdCaching();
+    System.out.println("test id caching generation: generated: " + idCaching);
+    assertNotNull(idCaching);
+  }
+
+}
