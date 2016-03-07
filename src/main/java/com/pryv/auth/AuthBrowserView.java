7ea92fc33de9b51c74b6553a303747028b9ddab2
ikebets <ilia.kebets@epfl.ch>
2016-03-07 18:16:46 +0100

    remove conflict

--- src/main/java/com/pryv/auth/AuthBrowserView.java
+++ src/main/java/com/pryv/auth/AuthBrowserView.java
@@ -17,7 +17,6 @@ import java.net.URL;
  */
 public class AuthBrowserView extends Applet implements AuthView {
 
-<<<<<<< HEAD
   public AuthBrowserView() {
   }
 
@@ -59,48 +58,5 @@ public class AuthBrowserView extends Applet implements AuthView {
   @Override
   public void onAuthRefused(int reasonId, String message, String detail) {
   }
-=======
-	public AuthBrowserView() {
-	}
-
-	public static void openWebpage(URI uri) {
-		Desktop desktop = Desktop.isDesktopSupported() ? Desktop.getDesktop() : null;
-		if (desktop != null && desktop.isSupported(Desktop.Action.BROWSE)) {
-			try {
-				desktop.browse(uri);
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
-	}
-
-	@Override
-	public void displayLoginView(String loginURL) {
-		try {
-			URL url = new URL(loginURL);
-			openWebpage(url.toURI());
-		} catch (URISyntaxException e) {
-			e.printStackTrace();
-		} catch (MalformedURLException e) {
-			e.printStackTrace();
-		}
-
-	}
-
-	@Override
-	public void onAuthSuccess(String username, String token) {
-		// TODO: called from AuthController
-	}
-
-	@Override
-	public void onAuthError(String message) {
-		// TODO: called from AuthController
-	}
-
-	@Override
-	public void onAuthRefused(int reasonId, String message, String detail) {
-		// TODO: called from AuthController
-	}
->>>>>>> 529d90ac357bc4d6db5d7b73c1093b558395cd06
 
 }
