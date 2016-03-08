1c021f28e7258929b509d45cfe690f4db2b41263
Thiebaud Modoux <thiebaud.modoux@epfl.ch>
2016-03-08 17:08:45 +0100

    Charset UTF8 api level fix

--- src/test/java/com/pryv/unit/JsonConverterTest.java
+++ src/test/java/com/pryv/unit/JsonConverterTest.java
@@ -7,6 +7,7 @@ import static org.junit.Assert.fail;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -168,7 +169,7 @@ public class JsonConverterTest {
       new File(getClass().getClassLoader().getResource("resources/testEvent.json").getPath());
     try {
       byte[] encoded = Files.readAllBytes(Paths.get(file.getAbsolutePath()));
-      String jsonEvent = new String(encoded, StandardCharsets.UTF_8);
+      String jsonEvent = new String(encoded, Charset.forName("UTF-8"));
       Event parsedEvent = JsonConverter.retrieveEventFromJson(jsonEvent);
       assertEquals(parsedEvent.getId(), "cib83213g002f1nmw1kspsrzn");
       assertEquals(parsedEvent.getStreamId(), "onlineModuleStreamID");
